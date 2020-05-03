ca9f53ed7b9ee4390f00bfd33cb5d200544b9e5f
Shubhasharda <shubham.sharda72@yahoo.com>
2020-05-03 23:24:21 +0530

    initial commit

---
+++ Models/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ekart.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int id { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string name { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string address { get; set; }
+        [Column(TypeName = "nvarchar(15)")]
+        public string phoneNumber { get; set; }
+        [Column(TypeName = "nvarchar(40)")]
+        public string email { get; set; }
+        [Column(TypeName = "nvarchar(20)")]
+        public string userName { get; set; }
+        [Column(TypeName = "nvarchar(40)")]
+        public string password { get; set; }
+    }
+}
---
+++ Models/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ekart.Models
+{
+    public class Item
+    {
+        public int id { get; set; }
+        public string itemName { get; set; }
+        public int price { get; set; }
+        public string description { get; set; }
+        public Reviews MyProperty { get; set; }
+    }
+}
